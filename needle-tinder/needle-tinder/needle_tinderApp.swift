4030798642042805b78c09c7b15ede9abb473861
paulinevivier <vivier.pauline@hotmail.fr>
2023-08-29 11:15:22 +0200

    project creation

---
+++ needle-tinder/needle-tinder/needle_tinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  needle_tinderApp.swift
+//  needle-tinder
+//
+//  Created by MacBook Pauline on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct needle_tinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
